6128f1f947f6b8340f4e0af5529d70c71586cf34
m3rc1k <maksimkakcmblcl@gmail.com>
2025-10-06 22:55:36 +0300

    Posts, Categories: models, serializers, views, urls

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     'apps.accounts.apps.AccountsConfig',
+    'apps.main.apps.MainConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
